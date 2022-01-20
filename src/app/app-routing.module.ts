6e57c02f512103ccfd39f297c34a08c877da0a90
4war <fedro_777@mail.ru>
2022-01-20 11:08:09 +0400

    20.01.2022 Понял router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {SectionSalesComponent} from "./Sections/section-sales/section-sales.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: SectionSalesComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {Routes, RouterModule} from "@angular/router";
+import {BrowserModule} from '@angular/platform-browser';
+import {appRoutes} from "../routes";
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {SectionSalesComponent} from './Sections/section-sales/section-sales.component';
+import {SectionOrdersComponent} from './Sections/section-orders/section-orders.component';
+import {SectionHealthComponent} from './Sections/section-health/section-health.component';
+import {NgChartsModule} from "ng2-charts";
+import { BarChartComponent } from './Charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './Charts/line-chart/line-chart.component';
+import { PieChartComponent } from './Charts/pie-chart/pie-chart.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    NgChartsModule
+  ],
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    SidebarComponent,
+    SectionSalesComponent,
+    SectionOrdersComponent,
+    SectionHealthComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
